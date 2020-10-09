69977c4b179fca80d19a1d23e8c61fc79e081472
Gustavo G <gustavogoullart@hotmail.com>
2020-10-09 14:50:39 -0300

    form fixed, some layout ajdusts

--- script/script.js
+++ script/script.js
@@ -2,28 +2,46 @@ const name = document.querySelector('#name');
 const email = document.querySelector('#email');
 const message = document.querySelector('#message');
 const sendButton = document.querySelector('#sendButton')
-const contactForm = document.querySelector('#contactForm');
+const form = document.querySelector('#contactForm');
 const inputRequired = document.querySelectorAll('.inputRequired');
+const status = document.querySelector('#statusForm')
+
+window.addEventListener("load", function () {
+    function success() {
+        form.reset();
+        status.classList.add('success')
+        status.innerHTML = "<p>Thanks (:</p>"
+
+    }
+
+    function error() {
+        status.classList.add('error')
+        status.innerHTML = "<p>Oops! We have an error :(</p>"
+        form.reset();
+    }
+
+    // handle the form submission event
+
+    form.addEventListener("submit", function (ev) {
+        ev.preventDefault();
+        var data = new FormData(form);
+        ajax(form.method, form.action, data, success, error);
+    });
+});
+
+function ajax(method, url, data, success, error) {
+    var xhr = new XMLHttpRequest();
+    xhr.open(method, url);
+    xhr.setRequestHeader("Accept", "application/json");
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState !== XMLHttpRequest.DONE) return;
+        if (xhr.status === 200) {
+            success(xhr.response, xhr.responseType);
+        } else {
+            error(xhr.status, xhr.response, xhr.responseType);
+        }
+    };
+    xhr.send(data);
+}
 
 
-
-// window.addEventListener('load', () => {
-//     contactForm.addEventListener('submit', evt => {
-//         evt.preventDefault();
-//     })
-// })
-
-
-// function formReady() {
-//     function handleTyping(evt) {
-//         return !!evt.target.value && evt.target.value.trim() !== '';
-//     }
-
-//     name.addEventListener('keyup', handleTyping);
-//     email.addEventListener('keyup', handleTyping);
-//     message.addEventListener('keyup', handleTyping);
-
-//     console.log(name)
-// }
-
-// formReady();
