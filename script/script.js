9a10de12820350c42e51ffe6f4843d896a289b47
Gustavo G <gustavogoullart@hotmail.com>
2020-10-12 12:13:35 -0300

    status form fixed

--- script/script.js
+++ script/script.js
@@ -6,36 +6,34 @@ const form = document.querySelector('#contactForm');
 const inputRequired = document.querySelectorAll('.inputRequired');
 const status = document.querySelector('#statusForm')
 
+
 window.addEventListener("load", function () {
+    form.addEventListener("submit", function (e) {
+        e.preventDefault();
+        var data = new FormData(form);
+        ajax(form.method, form.action, data, success, error);
+    });
+
     function success() {
         form.reset();
         status.classList.add('success')
         status.innerHTML = "<p>Thanks (:</p>"
-
     }
 
     function error() {
         status.classList.add('error')
         status.innerHTML = "<p>Oops! We have an error :(</p>"
-
-    }
-    function clean() {
-        if (status.classList.contains('success')) {
-            return status.classList.remove('success')
-        }
-        if (status.classList.contains('error')) {
-            return status.classList.remove('success')
-        }
     }
 
-    // handle the form submission event
-
-    form.addEventListener("submit", function (ev) {
-        ev.preventDefault();
-        var data = new FormData(form);
-        ajax(form.method, form.action, data, success, error);
-    });
 });
+
+function removeSpan(e) {
+    if (e.animationName !== 'status') return;
+    this.classList.remove('success', 'error');
+    status.innerHTML = '';
+}
+
+status.addEventListener('animationend', removeSpan);
 
 function ajax(method, url, data, success, error) {
     var xhr = new XMLHttpRequest();
